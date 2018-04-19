8de53412b1575062b0a4da33417afcd05533f924
ベース）陳　偉城 <fj5831ex@aa.jp.fujitsu.com>
2018-04-19 15:59:14 +0900

    port

--- src/test/java/com/qzyz/contact/ContactApplicationTests.java
+++ src/test/java/com/qzyz/contact/ContactApplicationTests.java
@@ -2,6 +2,7 @@ package com.qzyz.contact;
 
 import com.qzyz.contact.service.ContactImpl;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.formula.functions.T;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ContactApplicationTests {
 
@@ -20,6 +25,18 @@ public class ContactApplicationTests {
 	@Test
 	public void contextLoads() throws IOException {
 		contactImpl.update("陈伟城", "17091955633", "东京");
+		List<String> strList = new ArrayList<>();
+		strList.add("sss");
+
+		strList.forEach( (String e) -> System.out.println(e));
+
 	}
+
+	@Test
+	public void test() {
+
+
+	}
+
 
 }
