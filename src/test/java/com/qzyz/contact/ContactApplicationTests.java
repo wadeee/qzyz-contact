cedc56ac9fae9e60163c53735501e49ce524ee30
wade <wadechen@outlook.com>
2020-02-20 17:44:58 +0900

    test rm

--- src/test/java/com/qzyz/contact/ContactApplicationTests.java
+++ src/test/java/com/qzyz/contact/ContactApplicationTests.java
@@ -15,28 +15,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ContactApplicationTests {
 
-	@Autowired
-	ContactImpl contactImpl;
-
 	@Test
-	public void contextLoads() throws IOException {
-		contactImpl.update("陈伟城", "17091955633", "东京");
-		List<String> strList = new ArrayList<>();
-		strList.add("sss");
-
-		strList.forEach( (String e) -> System.out.println(e));
-
+	public void contextLoads() {
 	}
-
-	@Test
-	public void test() {
-
-
-	}
-
 
 }
